32f5a1b5ff19e889affce5b00ea7c4f899e97aac
stantushar <tushargeniusbuddy@gmail.com>
2023-07-25 10:51:45 +0530

    created java program to calculate factorial of number

---
+++ CalculateFactorial/src/app/Factorial.java
@@ -0,0 +1,24 @@
+package app;
+
+import java.util.Scanner;
+
+public class Factorial {
+
+		public static void main(String args[]) {
+			try(Scanner sc = new Scanner(System.in))
+			{
+				int i, fact = 1;
+				System.out.println("enter a number to find its factorial");
+				int number =  sc.nextInt();
+						for (i = 1; i <= number; i++) {
+							fact = fact * i;
+						}
+				System.out.println("Factorial of " + number + " is: " + fact);
+				
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	
+}
